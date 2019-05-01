2c5265f528724ec6f5dd4c8f98b1cdbf3a3b0f49
ariostokuit <32111541+ariostokuit@users.noreply.github.com>
2019-04-30 19:04:32 -0700

    Delete PixelPainter_Lab15_1.java

--- PixelPainter_Lab15_1.java
+++
@@ -1,8 +0,0 @@
-public class Lab15{
-    public static void main(String [] args){
-        PixelPainter frame = new PixelPainter();
-        frame.setVisible(true);
-        frame.setSize(425,200);
-        frame.setLocationRelativeTo(null);
-    }
-}
