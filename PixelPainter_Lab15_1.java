4e172cf813d5d351db8061624e2e7dc4e37c5132
ariostokuit <32111541+ariostokuit@users.noreply.github.com>
2019-04-30 19:04:14 -0700

    Add files via upload

---
+++ PixelPainter_Lab15_1.java
@@ -0,0 +1,8 @@
+public class Lab15{
+    public static void main(String [] args){
+        PixelPainter frame = new PixelPainter();
+        frame.setVisible(true);
+        frame.setSize(425,200);
+        frame.setLocationRelativeTo(null);
+    }
+}
